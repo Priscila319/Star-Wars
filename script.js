27b93ce350a313e647d108ac9b6b0bcb361953e1
Priscila <priscilabsmar@gmail.com>
2026-03-16 22:34:54 -0300

    adicionei Cor de olhos e fiz alterações no botão e nas linhas divisórias

--- script.js
+++ script.js
@@ -1,39 +1,3 @@
-/*async function getPersonagem() {
-
-    const idAleatorio = Math.floor(Math.random()*80) +1;
-    let response = await fetch(`https://swapi.dev/api/people/${idAleatorio}/`)
-    let data = await response.json()
-    console.log(data)
-
-
-
-    document.getElementById("resultado").innerText = data.name
-    console.log("O personagem sorteado foi: " + data.name)
-    document.getElementById("resultado").innerText = data.gender
-    console.log("O personagem sorteado foi: " + data.gender)
-;
-}  */
-
-
-/*
-async function getPersonagem() {
-  // Gera um ID aleatório entre 1 e 80
-  const idAleatorio = Math.floor(Math.random() * 80) + 1;
-  
-  try {
-    const response = await fetch(`https://swapi.dev/api/people/${idAleatorio}/`);
-    const data = await response.json();
-    
-    // Exibe o nome no console ou em algum elemento HTML
-    console.log("O personagem sorteado foi: " + data.name);
-    document.getElementById("resultado").innerText = data.name;
-    
-  } catch (error) {
-    console.error("Erro ao buscar dados:", error);
-  }
-}      */
-
-
 
 
   async function getPersonagem() {
@@ -43,11 +7,12 @@ async function getPersonagem() {
     
     console.log(data);
 
-    // Atualiza o nome no lugar certo
+
     document.getElementById("nome-personagem").innerText = data.name;
     
-    // Atualiza o gênero no lugar certo
     document.getElementById("genero-personagem").innerText = data.gender;
+
+    document.getElementById("cor-olhos").innerText = data.skin_color;
 
     console.log("O personagem sorteado foi: " + data.name);
 }
